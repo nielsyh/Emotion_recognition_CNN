ac3f9c39253df78ff406a1a66387ec908c49955d
Niels <info@nielshendrikx.nl>
2019-01-09 22:49:51 +0100

    rebuild, works, using dummy data MNIST

--- main.py
+++ main.py
@@ -1,313 +1,120 @@
-from network import *
-from help_functions import *
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from network import *
 
-# Source: https://github.com/Hvass-Labs/TensorFlow-Tutorials/blob/master/02_Convolutional_Neural_Network.ipynb
-# Tutorial: https://www.youtube.com/watch?v=HMcx-zY8JSg&t=159s
+#source https://pythonprogramming.net/cnn-tensorflow-convolutional-nerual-network-machine-learning-tutorial/
 
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-import numpy as np
-from sklearn.metrics import confusion_matrix
-import time
-from datetime import timedelta
 
 
-#setup layers
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
 
-# Convolutional Layer 1.
-filter_size1 = 3          # Convolution filters are 3 x 3 pixels.
-num_filters1 = 16         # There are 16 of these filters.
+#important settings
+keep_rate = 0.5 #dropout rate
+keep_prob = tf.placeholder(tf.float32)
+epochs = 3 # howmany dataset is pushed trough network... This is the slow part
+n_classes = 10
+batch_size = 128 # howmany samples at once trough network.
 
-# Convolutional Layer 2.
-filter_size2 = 3          # Convolution filters are 3 x 3 pixels.
-num_filters2 = 36         # There are 36 of these filters.
 
-# Convolutional Layer 3.
-filter_size3 = 3          # Convolution filters are 3 x 3 pixels.
-num_filters3 = 64         # There are 36 of these filters.
+def convolutional_neural_network(x):
+    weights = {
+        # 5 x 5 convolution, 1 input image, 32 outputs
+        'W_conv1': tf.Variable(tf.random_normal([3, 3, 1, 32])),
+        # 5x5 conv, 32 inputs, 64 outputs
+        'W_conv2': tf.Variable(tf.random_normal([3, 3, 32, 64])),
 
-# Fully-connected layer.
-fc_size = 128             # Number of neurons in fully-connected layer.
+        'W_conv3': tf.Variable(tf.random_normal([3, 3, 64, 128])),
 
+        # fully connected, 7*7*64 inputs, 1024 outputs. 4*4 because after applying 3 convolution and max-pooling operations, you are downsampling the input image from 28 x 28 x 1 to 4 x 4 x 1.
+        'W_fc': tf.Variable(tf.random_normal([4*4*128, 1024])),
+        # 1024 inputs, 10 outputs (class prediction)
+        'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    }
 
-from mnist import MNIST
-data = MNIST(data_dir="data/MNIST/")
+    biases = {
+        'b_conv1': tf.Variable(tf.random_normal([32])),
+        'b_conv2': tf.Variable(tf.random_normal([64])),
+        'b_conv3': tf.Variable(tf.random_normal([128])),
+        'b_fc': tf.Variable(tf.random_normal([1024])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
 
+    # Reshape input to a 4D tensor
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
 
-# The number of pixels in each dimension of an image.
-img_size = data.img_size
+    # Convolution Layer, using our function
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1'], biases['b_conv1']))
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1)
 
-# The images are stored in one-dimensional arrays of this length.
-img_size_flat = data.img_size_flat
+    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2'], biases['b_conv2']))
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2)
 
-# Tuple with height and width of images used to reshape arrays.
-img_shape = data.img_shape
+    conv3 = tf.nn.relu(conv2d(conv2, weights['W_conv3'], biases['b_conv3']))
+    # Max Pooling (down-sampling)
+    conv3 = maxpool2d(conv3)
 
-# Number of classes, one class for each of 10 digits.
-num_classes = data.num_classes
+    fc = tf.reshape(conv3, [-1, 4 * 4 * 128])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
+    fc = tf.nn.dropout(fc, keep_rate)
 
-# Number of colour channels for the images: 1 channel for gray-scale.
-num_channels = data.num_channels
 
-x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='x')
-x_image = tf.reshape(x, [-1, img_size, img_size, num_channels])
-y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
-y_true_cls = tf.argmax(y_true, axis=1)
+    output = tf.matmul(fc, weights['out']) + biases['out']
+    return output
 
-#conv layer 1
-layer_conv1, weights_conv1 =  new_conv_layer(input = x_image,
-                                             num_input_channels=num_channels,
-                                             filter_size = filter_size1,
-                                             num_filters= num_filters1,
-                                             use_pooling=True)
 
-#conv layer 2
-layer_conv2, weights_conv2 = new_conv_layer(input=layer_conv1,
-                   num_input_channels=num_filters1,
-                   filter_size=filter_size2,
-                   num_filters=num_filters2,
-                   use_pooling=True)
+def train_neural_network(x):
+    prediction = convolutional_neural_network(x)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits = prediction, labels=y)
+    cost = tf.reduce_mean(cross_entropy)
+    optimizer = tf.train.AdamOptimizer().minimize(cost) #default learning rate?
 
-#conv layer 3
-layer_conv3, weights_conv3 = new_conv_layer(input=layer_conv1,
-                   num_input_channels=num_filters2,
-                   filter_size=filter_size3,
-                   num_filters=num_filters3,
-                   use_pooling=True)
 
-#feed to flatning layers
-layer_flat, num_features = flatten_layer(layer_conv3)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-layer_fc1 = new_fc_layer(input=layer_flat,
-                         num_inputs=num_features,
-                         num_outputs=fc_size,
-                         use_relu=True)
+        train_loss = []
+        test_loss = []
+        train_acc = []
+        test_acc = []
 
-layer_fc2 = new_fc_layer(input=layer_fc1,
-                         num_inputs=fc_size,
-                         num_outputs=num_classes,
-                         use_relu=False)
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
+        for epoch in range(epochs):
+            epoch_loss = 0
 
-y_pred = tf.nn.softmax(layer_fc2)           #scale results with softmax
-y_pred_cls = tf.argmax(y_pred, axis=1)      #get class with highest prediciction
+            for b in range(int(mnist.train.num_examples / batch_size)):
 
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
-                                                        labels=y_true)
-cost = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
-correct_prediction = tf.equal(y_pred_cls, y_true_cls)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                opt = sess.run(optimizer, feed_dict={x: epoch_x, y: epoch_y})
 
+                loss, acc = sess.run([cost, accuracy], feed_dict={x: epoch_x,
+                                                                  y: epoch_y})
 
 
-session = tf.Session()
-session.run(tf.global_variables_initializer())
-train_batch_size = 64
+                #b, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                #epoch_loss += c
 
-# Counter for total number of iterations performed so far.
-total_iterations = 0
+            print("epoch " + str(epoch) + ", Loss= " + \
+                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.5f}".format(acc))
+            print("Optimization Finished!")
 
-def optimize(num_iterations):
-    # Ensure we update the global variable rather than a local copy.
-    global total_iterations
+            # Calculate accuracy for all 10000 mnist test images
+            test_accu, valid_loss = sess.run([accuracy, cost], feed_dict={x: epoch_x, y: epoch_y})
+            train_loss.append(loss)
+            test_loss.append(valid_loss)
+            train_acc.append(acc)
+            test_acc.append(test_accu)
 
-    # Start-time used for printing time-usage below.
-    start_time = time.time()
 
-    for i in range(total_iterations,
-                   total_iterations + num_iterations):
+        print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
-        # Get a batch of training examples.
-        # x_batch now holds a batch of images and
-        # y_true_batch are the true labels for those images.
-        x_batch, y_true_batch, _ = data.random_batch(batch_size=train_batch_size)
 
-        # Put the batch into a dict with the proper names
-        # for placeholder variables in the TensorFlow graph.
-        feed_dict_train = {x: x_batch,
-                           y_true: y_true_batch}
-
-        # Run the optimizer using this batch of training data.
-        # TensorFlow assigns the variables in feed_dict_train
-        # to the placeholder variables and then runs the optimizer.
-        session.run(optimizer, feed_dict=feed_dict_train)
-
-        # Print status every 100 iterations.
-        if i % 100 == 0:
-            # Calculate the accuracy on the training-set.
-            acc = session.run(accuracy, feed_dict=feed_dict_train)
-
-            # Message for printing.
-            msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
-
-            # Print it.
-            print(msg.format(i + 1, acc))
-
-    # Update the total number of iterations performed.
-    total_iterations += num_iterations
-
-    # Ending time.
-    end_time = time.time()
-
-    # Difference between start and end-times.
-    time_dif = end_time - start_time
-
-    # Print the time-usage.
-    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
-
-
-def plot_example_errors(cls_pred, correct):
-    # This function is called from print_test_accuracy() below.
-
-    # cls_pred is an array of the predicted class-number for
-    # all images in the test-set.
-
-    # correct is a boolean array whether the predicted class
-    # is equal to the true class for each image in the test-set.
-
-    # Negate the boolean array.
-    incorrect = (correct == False)
-
-    # Get the images from the test-set that have been
-    # incorrectly classified.
-    images = data.x_test[incorrect]
-
-    # Get the predicted classes for those images.
-    cls_pred = cls_pred[incorrect]
-
-    # Get the true classes for those images.
-    cls_true = data.y_test_cls[incorrect]
-
-    # Plot the first 9 images.
-    plot_images(images=images[0:9],
-                cls_true=cls_true[0:9],
-                cls_pred=cls_pred[0:9])
-
-
-def plot_confusion_matrix(cls_pred):
-    # This is called from print_test_accuracy() below.
-
-    # cls_pred is an array of the predicted class-number for
-    # all images in the test-set.
-
-    # Get the true classifications for the test-set.
-    cls_true = data.y_test_cls
-
-    # Get the confusion matrix using sklearn.
-    cm = confusion_matrix(y_true=cls_true,
-                          y_pred=cls_pred)
-
-    # Print the confusion matrix as text.
-    print(cm)
-
-    # Plot the confusion matrix as an image.
-    plt.matshow(cm)
-
-    # Make various adjustments to the plot.
-    plt.colorbar()
-    tick_marks = np.arange(num_classes)
-    plt.xticks(tick_marks, range(num_classes))
-    plt.yticks(tick_marks, range(num_classes))
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
-    plt.show()
-
-
-# Split the test-set into smaller batches of this size.
-test_batch_size = 256
-
-def print_test_accuracy(show_example_errors=False,
-                        show_confusion_matrix=False):
-
-    # Number of images in the test-set.
-    num_test = data.num_test
-
-    # Allocate an array for the predicted classes which
-    # will be calculated in batches and filled into this array.
-    cls_pred = np.zeros(shape=num_test, dtype=np.int)
-
-    # Now calculate the predicted classes for the batches.
-    # We will just iterate through all the batches.
-    # There might be a more clever and Pythonic way of doing this.
-
-    # The starting index for the next batch is denoted i.
-    i = 0
-
-    while i < num_test:
-        # The ending index for the next batch is denoted j.
-        j = min(i + test_batch_size, num_test)
-
-        # Get the images from the test-set between index i and j.
-        images = data.x_test[i:j, :]
-
-        # Get the associated labels.
-        labels = data.y_test[i:j, :]
-
-        # Create a feed-dict with these images and labels.
-        feed_dict = {x: images,
-                     y_true: labels}
-
-        # Calculate the predicted class using TensorFlow.
-        cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
-
-        # Set the start-index for the next batch to the
-        # end-index of the current batch.
-        i = j
-
-    # Convenience variable for the true class-numbers of the test-set.
-    cls_true = data.y_test_cls
-
-    # Create a boolean array whether each image is correctly classified.
-    correct = (cls_true == cls_pred)
-
-    # Calculate the number of correctly classified images.
-    # When summing a boolean array, False means 0 and True means 1.
-    correct_sum = correct.sum()
-
-    # Classification accuracy is the number of correctly classified
-    # images divided by the total number of images in the test-set.
-    acc = float(correct_sum) / num_test
-
-    # Print the accuracy.
-    msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
-    print(msg.format(acc, correct_sum, num_test))
-
-    # Plot some examples of mis-classifications, if desired.
-    if show_example_errors:
-        print("Example errors:")
-        plot_example_errors(cls_pred=cls_pred, correct=correct)
-
-    # Plot the confusion matrix, if desired.
-    if show_confusion_matrix:
-        print("Confusion Matrix:")
-        plot_confusion_matrix(cls_pred=cls_pred)
-
-
-print_test_accuracy()
-optimize(num_iterations=1)
-print_test_accuracy()
-optimize(num_iterations=99) # We already performed 1 iteration above.
-print_test_accuracy(show_example_errors=True)
-optimize(num_iterations=900) # We performed 100 iterations above.
-print_test_accuracy(show_example_errors=True)
-optimize(num_iterations=9000) # We performed 1000 iterations above.
-print_test_accuracy(show_example_errors=True,
-                    show_confusion_matrix=True)
-
-#notes
-# Do you get the exact same results if you run the Notebook multiple times without changing any parameters? What are the sources of randomness?
-# Run another 10,000 optimization iterations. Are the results better?
-# Change the learning-rate for the optimizer.
-# Change the configuration of the layers, such as the number of convolutional filters, the size of those filters, the number of neurons in the fully-connected layer, etc.
-# Add a so-called drop-out layer after the fully-connected layer. Note that the drop-out probability should be zero when calculating the classification accuracy, so you will need a placeholder variable for this probability.
-# Change the order of ReLU and max-pooling in the convolutional layer. Does it calculate the same thing? What is the fastest way of computing it? How many calculations are saved? Does it also work for Sigmoid-functions and average-pooling?
-# Add one or more convolutional and fully-connected layers. Does it help performance?
-# What is the smallest possible configuration that still gives good results?
-# Try using ReLU in the last fully-connected layer. Does the performance change? Why?
-# Try not using pooling in the convolutional layers. Does it change the classification accuracy and training time?
-# Try using a 2x2 stride in the convolution instead of max-pooling? What is the difference?
+train_neural_network(x)
