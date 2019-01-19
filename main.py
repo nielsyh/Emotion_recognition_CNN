928ccfe3bfbe30e6472dab61dc214b66edefd394
Your Name <you@example.com>
2019-01-19 22:11:48 +0100

    new result

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ img_rows, img_cols = 50, 50
 
 
 
-
 data = Data(41904)
 x_train = data.train_x #mnist.train.images
 x_test = data.test_x #mnist.test.images
@@ -50,13 +49,14 @@ model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-
 model.add(Dropout(0.25))
 #2
 model.add(Conv2D(64, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
+
 model.add(Dropout(0.25))
+
 #3
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -71,14 +71,15 @@ model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.SGD(lr=0.001),
-              metrics=['accuracy']
-              )
+              metrics=['accuracy'])
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test, y_test))
+          validation_split= 0.1)
 score = model.evaluate(x_test, y_test, verbose=0)
+
+
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
